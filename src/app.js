a693f590f0c0efaaaa9eab9dae5b212a753032b5
Jairo Soto <jairosoto05@gmail.com>
2021-03-20 01:04:01 -0400

    conexion a la base de datos y primera ruta terminada

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
+
+//settings
+app.set('port', process.env.PORT || 4000);
+
+//middleweres
+app.use(cors());
+app.use(express.json());
+
+//routes
+app.get('/users', (req, res) => res.send('Users Routes'))
+
 
 
 module.exports = app;
---
+++ src/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const DB = process.env.MONGODB_URI || 'mongodb://localhost:27017/db';
+
+mongoose.connect(DB,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+    
+});
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('db is connected');
+});
